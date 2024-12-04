5440ecccf1a21aeecece72fcd4808d8000cd0846
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-12-04 12:01:13 +0530

    created data for level

--- Array-Jumper/source/Level/LevelModel.cpp
+++ Array-Jumper/source/Level/LevelModel.cpp
@@ -2,5 +2,18 @@
 
 namespace Level
 {
+	LevelModel::LevelModel()
+	{
 
+	}
+
+	LevelModel::~LevelModel()
+	{
+
+	}
+
+	BlockType LevelModel::getCurrentBoxValue(int currentPosition)
+	{
+		return current_level_data.level_boxes[currentPosition];
+	}
 }
